7c1815ea2a05c59bb41f8666cec8f83c72c20050
NaolDaba <nahafile@gmail.com>
2025-08-06 13:39:41 +0300

    fix: update alias to support production build

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{jsx,tsx}", "./*.html"],
+    content: ["./src/**/*.{js,ts,jsx,tsx}", "./*.html"],
     theme: {
         extend: {
             colors: {
